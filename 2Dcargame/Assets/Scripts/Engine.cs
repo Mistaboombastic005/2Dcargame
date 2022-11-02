bfd5d462a1c7616d3c34426e10df4e73e2b27211
Mistaboombastic005 <tristanserraiocco2019@gmail.com>
2022-11-01 22:03:15 -0400

    Engine Update

--- 2Dcargame/Assets/Scripts/Engine.cs
+++ 2Dcargame/Assets/Scripts/Engine.cs
@@ -7,46 +7,79 @@ public class Engine : MonoBehaviour
     public float rpm;
     public float rpmLimit;
     public float idleRpm;
+    public int nbGears;
+    public int gearSelected;
+    public int highestGear;
+    public bool engineOn;
+    public bool engineStarting;
+    public float hp;
+
+
     public Rigidbody2D rb;
     public static float KM_H;
     public CameraFollow _cameraFollow;
-    public int gear;
-    public int numberOfGears;
-    public bool engineStarted;
+
+    public AudioSource audioSource;
+    public AudioClip starterSound;
+    public AudioClip idleSound;
 
 
 
-    private void Update()
+
+    private void Start()
     {
-        if (Input.GetKeyDown("x"))
-        {
-            gear++;
-        }
-
-        if (Input.GetKeyDown("z"))
-        {
-            gear--;
-        }
-
-        gear = Mathf.Clamp(gear, -1, numberOfGears);
-
-        if (Input.GetKeyDown("v") && !engineStarted)
-        {
-            engineStarted = true;
- 
-        }
-
-        if (Input.GetKeyDown("v") && engineStarted)
-        {
-            engineStarted = false;
-            
-        }
-
-        
-
-        
+        engineOn = false;
+        gearSelected = 0;
+        engineStarting = false;
     }
 
-    
-    
+    private void FixedUpdate()
+    {
+
+        if(Input.GetKeyDown("v"))// && (engineStarting = false))
+        {
+            if(engineOn == true)
+            {
+                engineOn = false;
+            }
+            else
+            {
+                engineStarting = true;
+            }
+            audioSource.clip = starterSound;
+            audioSource.Play();
+            StartCoroutine(PlaySound());
+        }
+
+        if (Input.GetKey("x"))
+        {
+            if(gearSelected < highestGear)
+            {
+                gearSelected += 1;
+            }
+        }
+        if(Input.GetKey("z"))
+        {
+            if(gearSelected > -1)
+            {
+                if (rpm < rpmLimit - 1000)
+                {
+                    gearSelected -= 1;
+                }
+            }
+        }
+        IEnumerator PlaySound()
+        {
+            audioSource.clip = starterSound;
+            audioSource.Play();
+            yield return new WaitForSeconds(starterSound.length);
+            engineStarting = false;
+            audioSource.clip = idleSound;
+            audioSource.Play();
+            audioSource.loop = true;
+        }
+    }
+
+
+
 }
