51fa3a6243b6e736731be44ee49bf7e9cf6104e7
FatBoy0658 <fatboy0658@gmail.com>
2023-01-07 22:43:38 -0500

    Dealership

---
+++ 2Dcargame/Assets/dealership.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dealership : MonoBehaviour
+{
+    
+    private void OnMouseDown()
+    {
+        Debug.Log("clicked");
+    }
+}
